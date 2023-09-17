f604d247a7691e31281b753b22ddf14762a9de6b
MidoriNoKen <midor1nok3n@gmail.com>
2023-09-17 16:05:12 +0700

    handler, routes and cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 // console.log('Halo semuanya, disini saya akan membuat RESTful API');
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      cores: {
+        origin: ['*'],
+      },
+    },
   });
-
+  server.route(routes);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
